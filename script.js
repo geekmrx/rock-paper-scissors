711ec6a5e1c841f7f399ae0617621a3f789cca18
Clauber <clauber.sportmg@gmail.com>
2022-12-11 22:08:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let forms = ["Rock", "Paper","Scissors"]
+function getComputerChoice() {
+    return forms[Math.floor(Math.random() * forms.length)]
+}
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(computerSelection)
+function playRound(playerSelection, computerSelection) {
+    let winner = ""
+    
+    return 
+}
+
+function game() {
+
+}
+
+console.log(playRound())
