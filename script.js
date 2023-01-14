0cfa625a4f61bfd595cd1f1a763a99763e2dbbf9
ClauberDev <clauber.dev@gmail.com>
2023-01-14 10:06:12 -0300

    desafio completo

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 
-let forms = ["Rock", "Paper","Scissors"]
-function getComputerChoice() {
-    return forms[Math.floor(Math.random() * forms.length)]
+const getComputerChoice = () => {
+    let shapes = ["rock", "paper", "scissors"]
+    return shapes[Math.floor(Math.random() * shapes.length)]
 }
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
-console.log(computerSelection)
-function playRound(playerSelection, computerSelection) {
-    let winner = ""
-    
-    return 
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === "rock" && computerSelection === "scissors" ||
+        computerSelection === "rock" && playerSelection === "scissors")
+        return "Rock beats scissors"
+    if (playerSelection === "scissors" && computerSelection === "paper" ||
+        computerSelection === "scissors" && playerSelection === "paper")
+        return "Scissors beat paper"
+    if (playerSelection === "paper" && computerSelection === "rock" ||
+        computerSelection === "paper" && playerSelection === "rock")
+        return "Paper beats rock."
+    else
+        return "Draw"
 }
 
-function game() {
-
-}
-
-console.log(playRound())
+const display = document.querySelector('.display')
+const buttons = document.querySelectorAll('button')
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        display.textContent = playRound(button.id, getComputerChoice())
+    })
+})
